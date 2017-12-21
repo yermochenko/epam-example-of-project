c64ef6150f0ecb88cbfc47296ff39bece7a807fe
Sergey Yermochenko <yermochenko@gmail.com>
2017-12-21 19:29:06 +0300

    refactoring
    generating of HTML was implemented in separate servlet (UserListView)

--- src/controller/UserListController.java
+++ src/controller/UserListController.java
@@ -1,7 +1,6 @@
 package controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
@@ -37,35 +36,8 @@ public class UserListController extends HttpServlet {
             UserServiceImpl service = new UserServiceImpl();
             service.setUserDao(dao);
             List<User> users = service.findAll();
-            resp.setCharacterEncoding("UTF-8");
-            PrintWriter writer = resp.getWriter();
-            writer.println("<!DOCTYPE html>");
-            writer.println("<html>");
-            writer.println("<head>");
-            writer.println("<meta charset=\"UTF-8\">");
-            writer.println("<title>Список пользователей</title>");
-            writer.println("<link href=\"../main.css\" rel=\"stylesheet\">");
-            writer.println("</head>");
-            writer.println("<body>");
-            writer.println("<h1>Банк «Рога и копыта»</h1>");
-            writer.println("<h2>Список пользователей</h2>");
-            writer.println("<table>");
-            writer.println("<tr>");
-            writer.println("<th>Имя пользователя</th>");
-            writer.println("<th>Роль</th>");
-            writer.println("<td>&nbsp;</td>");
-            writer.println("</tr>");
-            for(User user: users) {
-                writer.println("<tr>");
-                writer.printf("<td class=\"content\">%s</td>\n", user.getLogin());
-                writer.printf("<td class=\"content\">%s</td>\n", user.getRole());
-                writer.printf("<td class=\"empty\"><a href=\"edit.html?id=%d\" class=\"edit\"></a></td>\n", user.getId());
-                writer.println("</tr>");
-            }
-            writer.println("</table>");
-            writer.println("<a href=\"edit.html\" class=\"add-button\">Добавить</a>");
-            writer.println("</body>");
-            writer.println("</html>");
+            req.setAttribute("users", users);
+            req.getRequestDispatcher("/WEB-INF/user/list.html").forward(req, resp);
         } catch(SQLException | ServiceException e) {
             throw new ServletException(e);
         } finally {
