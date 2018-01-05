9412a8cb77f4baa188207f40080abbc0bd833d08
Sergey Yermochenko <yermochenko@gmail.com>
2018-01-06 01:04:42 +0300

    bug fix
    bug with saving cyrilic on forms was fixed

---
+++ src/controller/EncodingFilter.java
@@ -0,0 +1,24 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class EncodingFilter implements Filter {
+    @Override
+    public void init(FilterConfig config) throws ServletException {}
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+        req.setCharacterEncoding("UTF-8");
+        chain.doFilter(req, resp);
+    }
+
+    @Override
+    public void destroy() {}
+}
