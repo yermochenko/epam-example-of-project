5de2df215484188115091d478860f8ff67d6354a
Sergey Yermochenko <yermochenko@gmail.com>
2018-01-06 01:21:35 +0300

    new feature
    protection of resources from unauthorized access was implemented

---
+++ src/controller/SecurityFilter.java
@@ -0,0 +1,84 @@
+package controller;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import domain.Role;
+import domain.User;
+
+public class SecurityFilter implements Filter {
+    private static final Map<String, Set<Role>> permissions = new HashMap<>();
+
+    static {
+        Set<Role> all = new HashSet<>();
+        all.addAll(Arrays.asList(Role.values()));
+        Set<Role> admin = new HashSet<>();
+        admin.add(Role.ADMINISTRATOR);
+        Set<Role> employees = new HashSet<>();
+        employees.add(Role.MANAGER);
+        employees.add(Role.CASHIER);
+
+        permissions.put("/", null);
+        permissions.put("/index", null);
+        permissions.put("/login", null);
+
+        permissions.put("/logout", all);
+
+        permissions.put("/user/list", admin);
+        permissions.put("/user/edit", admin);
+        permissions.put("/user/save", admin);
+        permissions.put("/user/delete", admin);
+
+        permissions.put("/account/list", employees);
+        permissions.put("/account/view", employees);
+    }
+
+    @Override
+    public void init(FilterConfig config) throws ServletException {}
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpReq = (HttpServletRequest)req;
+        HttpServletResponse httpResp = (HttpServletResponse)resp;
+        String url = httpReq.getRequestURI();
+        String context = httpReq.getContextPath();
+        int postfixIndex = url.lastIndexOf(".html");
+        if(postfixIndex != -1) {
+            url = url.substring(context.length(), postfixIndex);
+        } else {
+            url = url.substring(context.length());
+        }
+        Set<Role> roles = permissions.get(url);
+        if(roles != null) {
+            HttpSession session = httpReq.getSession(false);
+            if(session != null) {
+                User user = (User)session.getAttribute("currentUser");
+                if(user != null && roles.contains(user.getRole())) {
+                    chain.doFilter(req, resp);
+                    return;
+                }
+            }
+        } else {
+            chain.doFilter(req, resp);
+            return;
+        }
+        httpResp.sendRedirect(context + "/login.html");
+    }
+
+    @Override
+    public void destroy() {}
+}
