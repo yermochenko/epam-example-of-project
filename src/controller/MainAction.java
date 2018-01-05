ebe720a75cb9ffca5530f18e95fc8b5a4562254a
Sergey Yermochenko <yermochenko@gmail.com>
2018-01-06 01:13:51 +0300

    new feature
    automatic opening of the start page for each role was implemented

--- src/controller/MainAction.java
+++ src/controller/MainAction.java
@@ -5,10 +5,31 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import domain.User;
 
 public class MainAction extends Action {
     @Override
     public Forward execute(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        return new Forward("/index", false);
+        HttpSession session = req.getSession(false);
+        if(session != null) {
+            User user = (User)session.getAttribute("currentUser");
+            if(user != null) {
+                switch(user.getRole()) {
+                case ADMINISTRATOR:
+                    return new Forward("/user/list.html");
+                case MANAGER:
+                case CASHIER:
+                    return new Forward("/account/list.html");
+                default:
+                    return new Forward("/login.html");
+                }
+            } else {
+                return new Forward("/login.html");
+            }
+        } else {
+            return new Forward("/login.html");
+        }
     }
 }
